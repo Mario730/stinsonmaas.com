cc087c67580485f2807d5f7eac34767392f03d13
Mario <mario55555@cox.net>
2022-12-18 23:48:29 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `stinsonmaas.com`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
